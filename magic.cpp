15599bf144743f2928ee17519524019e5a72fe3f
Revtm <kykylizaro98@gmail.com>
2019-09-28 11:15:02 +0700

    percobaan genetika

--- magic.cpp
+++ magic.cpp
@@ -17,17 +17,52 @@ struct isi{
   int e;
 };
 
+struct parent{
+  isi par1;
+  isi par2;
+};
+
 vector <isi> populasi;
 vector <int> P_fitness;
 vector <int> persen_fitness;
 
-void cetakPuzzle(int pzl[3][3]) {
+void cetakPuzzle(isi x) {
+  int tempPuzzle[3][3] = {
+  {0,0,8},
+  {9,0,1},
+  {0,7,0}
+  };
+
+
+  tempPuzzle[0][0] = x.a;
+  tempPuzzle[0][1] = x.b;
+  tempPuzzle[1][1] = x.c;
+  tempPuzzle[2][0] = x.d;
+  tempPuzzle[2][2] = x.e;
+
   for(int i=0; i<3; i++ ){
     for(int j=0; j<3; j++){
-      cout << pzl[i][j] << " ";
+      cout << tempPuzzle[i][j] << " ";
     }
     cout << endl;
   }
+}
+
+void cetakHasil(isi x){
+  int tempPuzzle[3][3] = {
+  {0,0,8},
+  {9,0,1},
+  {0,7,0}
+  };
+
+  tempPuzzle[0][0] = x.a;
+  tempPuzzle[0][1] = x.b;
+  tempPuzzle[1][1] = x.c;
+  tempPuzzle[2][0] = x.d;
+  tempPuzzle[2][2] = x.e;
+
+   // cetakPuzzle(tempPuzzle);
+   // cout << endl << endl;
 }
 
 void hitungPersenFitness(){
@@ -54,15 +89,19 @@ int hitungFitness(isi x){
   {0,7,0}
   };
 
-
+  int target[5]={4,3,5,2,6};
   tempPuzzle[0][0] = x.a;
   tempPuzzle[0][1] = x.b;
   tempPuzzle[1][1] = x.c;
   tempPuzzle[2][0] = x.d;
   tempPuzzle[2][2] = x.e;
 
-  cetakPuzzle(tempPuzzle);
-  cout << endl << endl;
+  int tempPuzz[5];
+  tempPuzz[0] = x.a;
+  tempPuzz[1] = x.b;
+  tempPuzz[2] = x.c;
+  tempPuzz[3] = x.d;
+  tempPuzz[4] = x.e;
 
   for(int i = 0 ; i < 3 ; i++){
     if(tempPuzzle[i][0] != tempPuzzle[i][1] &&  tempPuzzle[i][0] != tempPuzzle[i][2] && tempPuzzle[i][1] != tempPuzzle[i][2]){
@@ -106,27 +145,151 @@ int hitungFitness(isi x){
   }
   temp = 0;
 
-  cout  << "fitness: "<<total << endl;
+
+
+  cout  << "fitness: "<< total << endl;
+
   return total;
 }
 
-isi seleksi(){
-  int roda[100];
-  int temp = 0;
-  for(int i = 0 ; i < populasi.size() ; i++){
-    for(int j = temp ; j < temp + persen_fitness[i]  ; j++){
-      roda[j]=i;
+int carimax(){
+  int max = 0;
+  for(int i = 0 ; i < P_fitness.size() ; i++){
+    if(P_fitness[i] > max){
+      max = P_fitness[i];
     }
-    temp += persen_fitness[i];
   }
+  return max;
+}
 
-  srand(time(0));
-  isi x = populasi[roda[rand()%temp]];
+void sortingpop(){
+  int temp;
+  isi tempindiv;
+  for(int i=0; i< populasi.size()-1 ; i++){
+    for(int j=0 ; j < populasi.size()-i-1 ;j++){
+      if(P_fitness[j] < P_fitness[j+1]){
+        temp = P_fitness[j];
+        P_fitness[j]=P_fitness[j+1];
+        P_fitness[j+1]=temp;
 
+        tempindiv = populasi[j];
+        populasi[j]=populasi[j+1];
+        populasi[j+1]=tempindiv;
+      }
+    }
+  }
+}
+
+isi seleksi(int n){
+  // int roda[100];
+  // int temp = 0, temp2 = 0;
+  // for(int i = 0 ; i < populasi.size() ; i++){
+  //   if(persen_fitness[i] > 0){
+  //     temp2=temp + persen_fitness[i];
+  //     for(int j = temp ; j <= temp2+1  ; j++){
+  //       roda[j]=i;
+  //     }
+  //     temp += persen_fitness[i];
+  //   }
+  // }
+
+
+  srand(time(0)+n);
+  isi x ;
+  x= populasi[rand()%10];
+  //cout << "\njumlah temp: " <<temp<< endl << endl;
   return x;
 }
 
+int mutasi(){
 
+  int gen[5]={2,3,4,5,6};
+
+  srand(time(0));
+  return gen[rand()%5];
+
+}
+
+void crossover(){
+  parent parent1;
+
+  int temp, fitness;
+  sortingpop();
+  parent1.par1 = seleksi(2);
+  parent1.par2 = seleksi(6);
+
+  int mutasianak[5] = {parent1.par1.a, parent1.par1.b, parent1.par1.c, parent1.par1.d, parent1.par1.e};
+  int mutasianak2[5] = {parent1.par2.a, parent1.par2.b, parent1.par2.c, parent1.par2.d, parent1.par2.e};
+
+  for(int i = 0 ; i < 2 ; i++){
+    srand(time(0));
+    int r =rand()%5;
+    srand(time(0)+3);
+    int r2 =rand()%5;
+    temp = mutasianak[r];
+    mutasianak[r] = mutasianak2[r2];
+    mutasianak2[r2] = temp;
+
+  }
+
+  parent1.par1.a = mutasianak2[0];
+  parent1.par1.b = mutasianak2[1];
+  parent1.par1.c = mutasianak[2];
+  parent1.par1.d = mutasianak[3];
+  parent1.par1.e = mutasianak[4];
+
+  parent1.par2.a = mutasianak[0];
+  parent1.par2.b = mutasianak[1];
+  parent1.par2.c = mutasianak2[2];
+  parent1.par2.d = mutasianak2[3];
+  parent1.par2.e = mutasianak2[4];
+
+
+  int mutasianak3[5] = {parent1.par1.a, parent1.par1.b, parent1.par1.c, parent1.par1.d, parent1.par1.e};
+  int mutasianak4[5] = {parent1.par2.a, parent1.par2.b, parent1.par2.c, parent1.par2.d, parent1.par2.e};
+
+  for(int i = 0 ; i < 5 ;i++){
+    srand(time(0));
+    int prob = rand()%100;
+    if(prob < 45){
+      srand(time(0));
+      mutasianak3[rand()%5] = mutasi();
+      srand(time(0));
+      mutasianak4[rand()%5] = mutasi();
+    }
+  }
+
+  parent1.par1.a = mutasianak3[0];
+  parent1.par1.b = mutasianak3[1];
+  parent1.par1.c = mutasianak3[2];
+  parent1.par1.d = mutasianak3[3];
+  parent1.par1.e = mutasianak3[4];
+
+  parent1.par2.a = mutasianak4[0];
+  parent1.par2.b = mutasianak4[1];
+  parent1.par2.c = mutasianak4[2];
+  parent1.par2.d = mutasianak4[3];
+  parent1.par2.e = mutasianak4[4];
+
+  cout << "\n======cross-over==========\n";
+
+  populasi.push_back(parent1.par1);
+  fitness = hitungFitness(parent1.par1);
+  P_fitness.push_back(fitness);
+
+  populasi.push_back(parent1.par2);
+  fitness = hitungFitness(parent1.par2);
+  P_fitness.push_back(fitness);
+
+  cetakPuzzle(parent1.par1);
+  cout << endl;
+  cetakPuzzle(parent1.par2);
+  cout << endl;
+
+  persen_fitness.clear();
+  hitungPersenFitness();
+
+}
 
 isi buatIndividu(int acak){
   isi x;
@@ -209,17 +372,45 @@ void buatPopulasi(){
     populasi.push_back(x);
     fitness = hitungFitness(x);
     P_fitness.push_back(fitness);
+    cetakPuzzle(x);
   }
+  hitungPersenFitness();
+}
+
+bool cekFit(){
+  for(int i = 0 ; i < populasi.size() ; i ++){
+    if(P_fitness[i] == 5){
+      cout << "============================ \n hasil: \n";
+      cetakPuzzle(populasi[i]);
+      cout << endl;
+      return true;
+    }
+  }
+  return false;
 }
 
 int main(){
 isi x,y;
+  bool fitnesstarget = false;
 
   buatPopulasi();
+  // for(int i = 0 ; i < populasi.size() ; i++){
+  //   cout << P_fitness[i] << endl;
+  //   cout << populasi[i].a << populasi[i].b<<populasi[i].c<<populasi[i].d<<populasi[i].e << endl;
+  // }
+  //
+  // cout << "\n====================\n";
+   sortingpop();
+  // for(int i = 0 ; i < populasi.size() ; i++){
+  //   cout << P_fitness[i] << endl;
+  //   cout << populasi[i].a << populasi[i].b<<populasi[i].c<<populasi[i].d<<populasi[i].e << endl;
+  // }
 
-  cout << "===================\n";
-  cout << populasi[0].a << endl;
-  cout << P_fitness[0] << endl;
+  while(fitnesstarget == false){
+    sortingpop();
+    crossover();
+    fitnesstarget = cekFit();
+  }
 
   return 0;
 }
